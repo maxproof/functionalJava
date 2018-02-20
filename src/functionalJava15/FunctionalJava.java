29a45d733136a2e8858aab2b4c95f30a2d3031c0
maxproof <max.karberg@gmail.com>
2018-02-20 14:19:25 +0100

    5th commit

--- src/functionalJava15/FunctionalJava.java
+++ src/functionalJava15/FunctionalJava.java
@@ -1,12 +1,45 @@
 package functionalJava15;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 //STREAM
 
 public class FunctionalJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		List<String> list = new ArrayList<>();
+		list.add("Max");
+		list.add("Gertrude");
+		list.add("Jean-François"); 
+		list.add("Patt");
+		
+		list
+			.stream()
+			.filter(s -> s.length() > 5)
+			.forEach(System.out::println);
+			
+		Stream.of("Max", "Gertrude", "Patt", "Jean-François")
+			.filter(s -> s.length() <5)
+			.map(String::toUpperCase)
+			.forEach(System.out::println);
+		
+		Stream.of(5, 3, 7,2)
+			.filter(s -> s < 5)
+			.forEach(System.out::println);
+		
+		Optional<Integer> sum = Stream.of("Max", "Gertrude", "Patt", "Jean-François")
+			.filter(s -> s.length() <5)
+			.map(s -> s.length())
+			.reduce(
+					(n1, n2) -> n1 + n2
+					);
+		
+		System.out.println("sum: " + sum.get());
+		
 	}
 
 }
